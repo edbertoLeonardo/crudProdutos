865423aaa61ed16eae935c32eb7ab49581c548dd
edbertoLeonardo <edbleonardo@gmail.com>
2022-01-26 21:01:24 -0300

    component criar produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 import {HomeComponent} from './views/home/home.component';
 import { ProdutoCrudComponent } from './views/produto-crud/produto-crud.component';
+
 
 const routes: Routes = [{
   path: "",
@@ -10,6 +12,9 @@ const routes: Routes = [{
 },{
   path: "produtos",
   component: ProdutoCrudComponent
+},{
+  path: "products/create",
+  component: ProductCreateComponent
 }];
 
 @NgModule({
