a41860b92b93d5f7dca3fef17e9cf844f8bac511
edbertoLeonardo <edbleonardo@gmail.com>
2022-01-27 09:16:25 -0300

    navegar para tela criar produtos

--- src/app/views/produto-crud/produto-crud.component.ts
+++ src/app/views/produto-crud/produto-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-produto-crud',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoCrudComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+
+   }
 
   ngOnInit(): void {
   }
 
+  irParaProdutoCreate(): void {
+  this.router.navigate(['/products/create'])
+  }
 }
