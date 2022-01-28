ffdcd9d3e86558393fd5d2ee14aec8d997eb21b0
edbertoLeonardo <edbleonardo@gmail.com>
2022-01-28 19:40:51 -0300

    atualizando titulo e icone

--- src/app/views/produto-crud/produto-crud.component.ts
+++ src/app/views/produto-crud/produto-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-produto-crud',
@@ -8,8 +9,12 @@ import { Router } from '@angular/router';
 })
 export class ProdutoCrudComponent implements OnInit {
 
-  constructor(private router: Router) {
-
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
    }
 
   ngOnInit(): void {
